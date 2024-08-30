454ae9bd9cf096c88bc5eb52e6295fb1f0bbf294
hedeks <103194276+hedeks@users.noreply.github.com>
2024-08-30 00:10:46 +0000

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ const routes = {
           <div class="second-gativus-view-section-main-part">
             <div class="second-gativus-view-section-main-part-top">
               <!-- wtg = what is the gativus -->
-              <div class="gtom wtg_view_item" id="gtom">
+              <div class="gtom wtg_view_item" id="GTOM">
                 <div class="wtg_view_img_container wtg_gtom"></div>
                 <div class="wtg_view_title">
                   <b>G</b>ativus <b>T</b>heory <b>O</b>f <b>M</b>ind -
@@ -39,16 +39,16 @@ const routes = {
                   human conscience. From cellular level to the knowledge.
                 </div>
               </div>
-              <div class="gnet wtg_view_item" id="ganm">
+              <div class="gnet wtg_view_item" id="GANM">
                 <div class="wtg_view_img_container wtg_gnet_spec"></div>
                 <div class="wtg_view_title">
-                  <b>GA</b>tivus <b>N</b>etwork <b>M</b>odel - <b>GANM</b>
+                  <b>G</b>ativus <b>N</b>etwork <b>M</b>odel <b>S</b>pecification - <b>GNMS</b>
                 </div>
                 <div class="wtg_view_description">
                   Specification of GNET, detailed requirements description, constructions and functionality.
                 </div>
               </div>
-              <div class="gate wtg_view_item" id="gate">
+              <div class="gate wtg_view_item" id="GATE">
                 <div class="wtg_view_img_container wtg_gate"></div>
                 <div class="wtg_view_title">
                   <b>GAT</b>ivus <b>E</b>dge - <b>GATE</b>
@@ -58,7 +58,7 @@ const routes = {
                   entities.
                 </div>
               </div>
-              <div class="gnet wtg_view_item" id="gnet">
+              <div class="gnet wtg_view_item" id="GNET">
                 <div class="wtg_view_img_container wtg_gnet"></div>
                 <div class="wtg_view_title">
                   <b>G</b>ativus <b>NET</b>work - <b>GNET</b>
@@ -133,16 +133,17 @@ const routes = {
         <div class="fit-content-gativus-network-card">
           <div class="gativus-network-card-title-container">
             <h2 class="gativus-network-card-title">
-              <b>Gativus</b> has a vision of artificial intelligence based on
-              <u>nature of human consciousness.</u>
+              <b>Gativus Theory of Mind</b> has a vision of artificial
+              intelligence based on
+              nature of human consciousness.
             </h2>
           </div>
 
           <div class="fit-content-gativus-network-card-main">
             <p class="gativus-card-subtitle">
               Most fundamental is the fact, that human’s brain simultaneously
-              host two quite different networks: <b>neural network</b> and <b>concept
-              network.</b>
+              host two quite different networks: <b>neural network</b> and
+              <b>concept network.</b>
             </p>
             <div class="gativus-card-image-block">
               <div class="gativus-card-image">
@@ -163,9 +164,180 @@ const routes = {
               description).
             </p>
             <p class="gativus-card-subtitle">
-              Gativus is making attempt to <b>converge these two networks into</b>
-              <u>unified common network</u> and give the researches <b>an opportunity to
-              find association between</b> <u>neural</u> and <u>concept</u> layers.
+              Gativus is making attempt to
+              <b>converge these two networks into</b>
+              <u>unified common network</u> and give the researches
+              <b>an opportunity to find association between</b>
+              <u>neural</u> and <u>concept</u> layers.
+            </p>
+          </div>
+        </div>
+        <img src="./img/two-arrows-down.svg" alt="" />
+        <div class="fit-content-gativus-network-card">
+          <div class="gativus-network-card-title-container">
+            <h2 class="gativus-network-card-title">
+              <b>G</b>ativus <b>L</b>ow <b>L</b>evel <b>N</b>etwork -
+              <b>GLLN</b>
+            </h2>
+          </div>
+          <div class="fit-content-gativus-network-card-main">
+            <p class="gativus-card-subtitle">
+              <b>Gativus Theory of Mind</b> outlines <b>GLLN</b> - three
+              low-level networks (associated with neural networks term),
+              comprising of S-V-A components:
+            </p>
+            <div class="gtom-glln-cards">
+              <div class="gtom-card">
+                <p>S - Structure (Cell information)</p>
+                <p>
+                  Cell information, actually coded by DNA, propagated and
+                  further creating cellular network;
+                </p>
+                <hr width="100%" class="hr" />
+                <p style="height: 100px">
+                  We have formed an understanding of the existence of a cellular
+                  network, having a specific description using
+                  <b>S-components.</b>
+                </p>
+              </div>
+              <div class="gtom-card">
+                <p>V - Value (Learning Parameters)</p>
+                <p>
+                  Learning parameters, which would modify behavior of the cell.
+                </p>
+                <hr width="100%" class="hr" />
+                <p style="height: 100px">
+                  The created cellular network is capable of receiving and
+                  transmitting signals <b>(V values)</b> using corresponding
+                  <b>V-relations.</b> In a sense, this is analogous to a memory
+                  cell and an electrical wire connecting the output to the
+                  input.
+                </p>
+              </div>
+              <div class="gtom-card">
+                <p>A - Activity (Spikes)</p>
+                <p>
+                  Spikes between axons and dendrites, creating another network
+                  above cellular one;
+                </p>
+                <hr width="100%" class="hr" />
+                <p style="height: 100px">
+                  To ensure the processing of inputs, we have
+                  <b>A-components</b>
+                  (activities) - analogues of code (programs) that can be
+                  executed by an existing data processor.
+                </p>
+              </div>
+            </div>
+            <p class="gativus-card-subtitle"><b>Additions:</b></p>
+            <p class="gativus-card-text">
+              Only spike network have the association of electrical conductivity
+              of signals, and would be seemed as something similar to electrical
+              networks relaying signals through the network. As soon as spikes
+              signify the value of the cell’s function – they are considered as
+              of the value and defined as v-relations in Gativus terms.
+            </p>
+            <p class="gativus-card-text">
+              But at the same time, structural code – even though is not
+              conducted in spike-like way, but still propagates the network.
+              Therefore, structure data or S-relation, in Gativus terms, is
+              considered to be a virtual delivery of structure construction data
+              to the hosting environment, allowing growth virtual (digital)
+              cell.
+            </p>
+            <p class="gativus-card-text">
+              Some values, such as dendrites weights, can be defined as a
+              content of internal containers, which would be changed or
+              memorized as a process of learning. As soon as Gativus already
+              introduced value containers, these learning data can be stored in
+              those containers. However, there is no v-relation, but new
+              capability of the cell.
+            </p>
+          </div>
+        </div>
+        <img src="./img/two-arrows-down.svg" alt="" />
+        <div class="fit-content-gativus-network-card">
+          <div class="gativus-network-card-title-container">
+            <h2 class="gativus-network-card-title">
+              <b>G</b>ativus <b>CO</b>gnitive <b>N</b>etwork - <b>GCON</b>
+            </h2>
+          </div>
+          <div class="fit-content-gativus-network-card-main">
+            <p class="gativus-card-subtitle">
+              GTOM considers human mind based on a few foundations:
+            </p>
+            <ul class="gativus-ul" style="margin: auto">
+              <li class="gativus-card-text">
+                <u>knowledge phenomenon</u>, as a primary foundation;
+              </li>
+              <li class="gativus-card-text">
+                <u>time-space paradigm</u>, as a perception feature/interface.
+              </li>
+            </ul>
+          </div>
+          <div class="gativus-network-card-another-title-container">
+            <h2 class="gativus-network-card-another-title">Knowledge</h2>
+          </div>
+
+          <div class="fit-content-gativus-network-card-main">
+            <p class="gativus-card-text">
+              Existence of knowledge entities (concepts) – is a fact.
+            </p>
+            <p class="gativus-card-text">
+              Knowledge entities KLEN form GCON (Gativus Cognitive network) – as
+              sub-network of GNET.
+            </p>
+            <p class="gativus-card-subtitle">
+              GTOM consider that Knowledge entities KLEN are based on RDF’s
+              triples, with some extensions:
+            </p>
+            <ul class="gativus-card-text" class="gativus-ul">
+              <li>
+                the predicate – would be various types, and represents Targets,
+                Activities or Events;
+              </li>
+              <li>
+                Subjects and Objects – would be numerous in one entity.
+                Technically, they are not included into KLEN, but taking part in
+                it by special participation relation (PARR), which are the part
+                of KLEN;
+              </li>
+              <li>
+                KLEN also includes sequence relations (SEQR) between Targets;
+              </li>
+              <li>
+                KLEN has a sequence status: future, present, and past. The
+                sequence status is the only time association in GTOM.
+              </li>
+            </ul>
+            <p class="gativus-card-subtitle"><b>Additions:</b></p>
+            <p class="gativus-card-text">
+              Technically, the Knowledge entity – is the behavior, which changes
+              the existing network by amending of existing or creating a new
+              entities (subjects or targets). The changes are relayed by PARR
+              relations.
+            </p>
+            <p class="gativus-card-text">
+              The identification of KLEN – is same of NDDI containing the
+              T-component. Creation of a New concept (target or subject) could
+              result in creation the new NDDI with corresponding T/C components,
+              or corresponding components can be hosted in the same NDDI.
+            </p>
+          </div>
+
+          <div class="gativus-network-card-another-title-container">
+            <h2 class="gativus-network-card-another-title">Time-space</h2>
+          </div>
+
+          <div class="fit-content-gativus-network-card-main">
+            <p class="gativus-card-text">
+              Gativus GTOM considers time/space as interface for knowledge
+              exchange between two or more subjects with consciousness (like
+              homo sapiens). The basic prototype is based on UML: class and
+              activity diagrams. Activity diagram is associated with sequence
+              relation SEQR between Targets, while class diagram can be seen as
+              participation relations PARR between subject Concepts and
+              consuming them Targets.
             </p>
           </div>
         </div>
@@ -360,7 +532,7 @@ const routes = {
               sections
             </p>
           </div>
-          <div class="nddi-sections">
+          <!-- <div class="nddi-sections"> -->
             <div class="nddi-vbs">
               <div class="nddi-vbs-info">
                 <div class="nddi-vbs-info-left">
@@ -469,17 +641,74 @@ const routes = {
                 </div>
               </div>
             </div>
-          </div>
+          <!-- </div> -->
         </div>
       </div>`,
   },
   "./gate": {
     linkLabel: "GATE",
-    content: `GATE`,
+    content: `<div class="gativus-page-container-center" style="width: 500px"> 
+                <div class="fit-content-gativus-network-card">
+          <div class="gativus-network-card-title-container">
+            <h2 class="gativus-network-card-title">
+              Is in the development stage
+            </h2>
+          </div>
+          <div class="fit-content-gativus-network-card-main">
+           <img src="./img/in_dev.png" height="200px" alt="" style="filter: saturate(0)">
+          </div>
+        </div>`,
   },
   "./gaar": {
     linkLabel: "GAAR",
-    content: `GAAR`,
+    content: `<div class="gativus-page-container-center" style="width: 500px"> 
+                <div class="fit-content-gativus-network-card">
+          <div class="gativus-network-card-title-container">
+            <h2 class="gativus-network-card-title">
+              Is in the development stage
+            </h2>
+          </div>
+          <div class="fit-content-gativus-network-card-main">
+           <img src="./img/in_dev.png" height="200px" alt="" style="filter: saturate(0)">
+          </div>
+        </div>`,
+  },
+  "./contact": {
+    linkLabel: "contact_us_link",
+    content: `<div class="gativus-page-container-center"> 
+                <div class="fit-content-gativus-network-card">
+          <div class="gativus-network-card-title-container">
+            <h2 class="gativus-network-card-title">
+              If you have any questions or suggestions, you can contact us:
+            </h2>
+          </div>
+
+          <div class="fit-content-gativus-network-card-main">
+            <div class="contact-email-block">
+              <img src="./img/logo.jpg" class="logo-section-img" alt="" />  
+              <p class="gativus-card-subtitle">
+              Email: contact@gativus.com
+              </p>
+            </div>
+          </div>
+          <div class="gativus-network-card-title-container_w_t_border">
+          <h2 class="gativus-network-card-title">
+          You can also fill out the form below to send us a message.
+          </h2>
+          </div>
+          <div class="fit-content-gativus-network-card-main">
+            <form class="contact-form">
+              <label for="email">Your name:</label>
+              <input class="contact-form-input" type="email" id="email" name="email" required>
+              <label for="email">Your email:</label>
+              <input class="contact-form-input" type="email" id="email" name="email" required>
+              <label for="message">Message:</label>
+              <textarea class="contact-form-input" id="message" name="message" required></textarea>
+              <input class="contact-form-input-button" type="submit" value="Contact Us">
+            </form>
+          </div>
+        </div>
+               </div>`,
   },
 };
 
@@ -488,63 +717,103 @@ const nav = document.querySelector("#nav");
 
 const renderContent = (route) => {
   app.innerHTML = routes[route].content;
+  if (route === "./"){
+    document.querySelectorAll(".wtg_view_item").forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        item.classList.add("hovered");
+      });
+      item.addEventListener("mouseout", () => {
+        item.classList.remove("hovered");
+      });
+      item.addEventListener('click', ()=>{
 
-  document.querySelectorAll(".wtg_view_item").forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      item.classList.add("hovered");
+        if (item.id === "GANM"){
+          window.location.href = "https://zh.wiki.gativus.com/index.php/GNET";
+        }
+        document.querySelectorAll('.header-link').forEach((link_item)=>{
+          if (link_item.id === item.id){
+            link_item.click();
+          }
+        })
+      })
     });
-    item.addEventListener("mouseout", () => {
-      item.classList.remove("hovered");
-    });
-  });
+  }
 };
-const navigate = (e) => {
+const navigate = (url) => {
+  if (url === getRouteByUrl(window.location.href)){
+    return;
+  }
   let particles = document.querySelector("#particles-js");
-  const route = `./${e.target.id.toLowerCase()}`;
-  window.history.pushState({}, "", route);
+  window.history.pushState({}, "", url);
   app.style.opacity = 0;
   app.style.transform = "translateY(-3px)";
   particles.style.opacity = 0;
   setTimeout(() => {
     scrollTo({ top: 0 });
-    renderContent(route);
+    renderContent(url);
     app.style.transform = "translateY(0)";
     app.style.opacity = 1;
     particles.style.opacity = 1;
   }, 500);
 };
 
-const registerNavLinks = () => {
-  nav.addEventListener("click", (e) => {
-    console.log(e.target);
+const registerPageScripts = () => {
+  document.querySelector(".gativus-header").addEventListener("click", (e) => {
     if (e.target.classList.contains("header-link")) {
       e.preventDefault();
       const { id } = e.target;
-      window.history.pushState({}, "", `./${id.toLowerCase()}`);
+      let url = "";
+      for (route in routes) {
+        if (routes[route].linkLabel === id) {
+          url = route;
+        }
+      }
       document.querySelectorAll(".header-link").forEach((item) => {
         item.classList.remove("active-link");
       });
       e.target.classList.add("active-link");
-      navigate(e);
+      navigate(url);
+    } else if (e.target.parentElement.classList.contains("header-link")) {
+      e.preventDefault();
+      const { id } = e.target.parentElement;
+      let url = "";
+      for (route in routes) {
+        if (routes[route].linkLabel === id) {
+          url = route;
+        }
+      }
+      document.querySelectorAll(".header-link").forEach((item) => {
+        item.classList.remove("active-link");
+      });
+      e.target.parentElement.classList.add("active-link");
+      navigate(url);
     }
   });
 };
 
+const getRouteByUrl = (url) => {
+    let start_index = url.lastIndexOf('/');
+    let str = url.substring(start_index, url.length);
+    return "." + str;
+}
+
+
 const registerBrowserBackAndForth = () => {
   window.onpopstate = function (e) {
-    const route = window.location.pathname;
+    const url = window.location.pathname;
+    const route = getRouteByUrl(url);
     renderContent(route);
   };
 };
 
 const renderInitialPage = () => {
-  const route = window.location.pathname;
-  console.log(route);
-  renderContent("./");
+  const url = window.location.pathname;
+  const route = getRouteByUrl(url);
+  renderContent(route);
 };
 
 (function bootup() {
-  registerNavLinks();
+  registerPageScripts();
   registerBrowserBackAndForth();
   renderInitialPage();
 })();
